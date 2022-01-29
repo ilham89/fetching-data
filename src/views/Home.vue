847e96e256554e5f224418088d63f280adcd526f
ilham89 <ilham.kodingworks@gmail.com>
2022-01-29 23:12:48 +0700

    feat: ✨ simple fetch data with axios

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,49 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+    <header>
+      <h4>Belajar fetch data</h4>
+    </header>
+    <div class="list">
+      <div v-if="load">Loading ...</div>
+      <ul v-for="todo in todos" :key="todo.id" style="list-style: none">
+        <li>
+          <router-link :to="{ name: 'Todo', params: { id: todo.id } }">{{
+            todo.title
+          }}</router-link>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  components: {},
+  data() {
+    return {
+      todos: [],
+      load: true,
+    };
+  },
+  created() {
+    this.getTodos();
+  },
+  methods: {
+    getTodos() {
+      axios
+        .get(`https://jsonplaceholder.typicode.com/todos`)
+        .then((res) => {
+          this.todos = res.data;
+        })
+        .catch((err) => {
+          alert(err);
+        })
+        .finally(() => (this.load = false));
+    },
   },
 };
 </script>
---
+++ src/views/Todo.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="todo">
+    <h2>Detail todo</h2>
+    <h4>{{ todo?.title }}</h4>
+    <div v-if="load">Loading ...</div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      todo: null,
+      load: true,
+    };
+  },
+  created() {
+    this.getTodo();
+  },
+  methods: {
+    getTodo() {
+      axios
+        .get(
+          `https://jsonplaceholder.typicode.com/todos/${this.$route.params.id}`
+        )
+        .then((res) => (this.todo = res.data))
+        .catch((err) => alert(err))
+        .finally(() => (this.load = false));
+    },
+  },
+};
+</script>
